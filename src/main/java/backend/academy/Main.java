3c8baa020ec3aab8ae86de1f7764cf3a0138d5c7
Konstantin Vikhlyantsev <vihlancevk@gmail.com>
2024-11-21 17:39:59 +0300

    Add support for symmetry parameter

--- src/main/java/backend/academy/Main.java
+++ src/main/java/backend/academy/Main.java
@@ -7,14 +7,15 @@ import backend.academy.transformation.implementation.Linear;
 import backend.academy.transformation.implementation.Sinusoidal;
 import backend.academy.transformation.implementation.Spherical;
 import backend.academy.transformation.implementation.Swirl;
+import backend.image.processor.ImageProcessor;
+import backend.image.processor.implementation.LogarithmicGammaCorrection;
 import java.nio.file.Path;
 import java.util.List;
 import java.util.Random;
-import backend.image.processor.ImageProcessor;
-import backend.image.processor.implementation.LogarithmicGammaCorrection;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
+@SuppressWarnings("MagicNumber")
 public class Main {
     public static void main(String[] args) {
         int width = 1000;
@@ -37,12 +38,14 @@ public class Main {
         long seed = 100L;
         Random random = new Random(seed);
 
-        double w = 0.1;
-        double h = 0.1;
-        for (double y = -1; Double.compare(y + h, 1.0) < 0; y += h) {
-            for (double x = -1; Double.compare(x + w, 1.0) < 0; x += w) {
-                Rect world = new Rect(x, y, w, h);
-                render(canvas, world, affines, variations, samples, iterPerSample, random);
+        int symmetry = 4;
+        boolean isRelativeSymmetry = false;
+
+        double l = 0.1;
+        for (double y = -1; Double.compare(y + l, 1.0) < 0; y += l) {
+            for (double x = -1; Double.compare(x + l, 1.0) < 0; x += l) {
+                Rect world = new Rect(x, y, l, l);
+                render(canvas, world, affines, variations, samples, iterPerSample, random, symmetry, isRelativeSymmetry);
             }
         }
 
@@ -59,7 +62,10 @@ public class Main {
         List<Transformation> variations,
         int samples,
         short iterPerSample,
-        Random random) {
+        Random random,
+        int symmetry,
+        boolean isRelativeSymmetry
+    ) {
         for (int num = 0; num < samples; num++) {
             Point point = world.getRandomPoint(random);
 
@@ -69,26 +75,33 @@ public class Main {
 
                 point = affine.apply(point);
                 point = variation.apply(point);
-                if (!world.contains(point)) {
-                    continue;
-                }
 
-                Pixel pixel = canvas.pixel(point.x(), point.y());
-                if (pixel == null) {
-                    continue;
-                }
+                double angle = 0;
+                for (int s = 0; s < symmetry; angle += Math.PI * 2 / symmetry, s++) {
+                    point = isRelativeSymmetry
+                        ? rotateRelativeSymmetry(world, point, angle)
+                        : rotateAbsoluteSymmetry(point, angle);
+                    if (!world.contains(point)) {
+                        continue;
+                    }
 
-                if (pixel.getHitCount() == 0) {
-                    pixel.setR(affine.getRgbR());
-                    pixel.setG(affine.getRgbG());
-                    pixel.setB(affine.getRgbB());
-                } else {
-                    pixel.setR((pixel.getR() + affine.getRgbR()) / 2);
-                    pixel.setG((pixel.getG() + affine.getRgbG()) / 2);
-                    pixel.setB((pixel.getB() + affine.getRgbB()) / 2);
+                    Pixel pixel = canvas.pixel(point.x(), point.y());
+                    if (pixel == null) {
+                        continue;
+                    }
+
+                    if (pixel.getHitCount() == 0) {
+                        pixel.setR(affine.getRgbR());
+                        pixel.setG(affine.getRgbG());
+                        pixel.setB(affine.getRgbB());
+                    } else {
+                        pixel.setR((pixel.getR() + affine.getRgbR()) / 2);
+                        pixel.setG((pixel.getG() + affine.getRgbG()) / 2);
+                        pixel.setB((pixel.getB() + affine.getRgbB()) / 2);
+                        pixel.setHitCount(pixel.getHitCount() + 1);
+                    }
                     pixel.setHitCount(pixel.getHitCount() + 1);
                 }
-                pixel.setHitCount(pixel.getHitCount() + 1);
             }
         }
     }
@@ -96,5 +109,29 @@ public class Main {
     private static <T> T getRandomElement(List<T> elements, Random random) {
         int randomIndex = random.nextInt(elements.size());
         return elements.get(randomIndex);
+    }
+
+    private static Point rotateAbsoluteSymmetry(Point point, double angle) {
+        double absoluteCenterX = 0;
+        double absoluteCenterY = 0;
+
+        return rotate(point, angle, absoluteCenterX, absoluteCenterY);
+    }
+
+    private static Point rotateRelativeSymmetry(Rect world, Point point, double angle) {
+        double relativeCenterX = world.x() + world.width() / 2;
+        double relativeCenterY = world.y() + world.height() / 2;
+
+        return rotate(point, angle, relativeCenterX, relativeCenterY);
+    }
+
+    private static Point rotate(Point point, double angle, double centreX, double centreY) {
+        double relativeX = point.x() - centreX;
+        double relativeY = point.y() - centreY;
+
+        double relativeRotatedX = relativeX * Math.cos(angle) - relativeY * Math.sin(angle);
+        double relativeRotatedY = relativeX * Math.sin(angle) + relativeY * Math.cos(angle);
+
+        return new Point(relativeRotatedX + centreX, relativeRotatedY + centreY);
     }
 }
